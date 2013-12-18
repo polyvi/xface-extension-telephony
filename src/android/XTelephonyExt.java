2e1f497dce4203d356e11e70f85c6502ecce45e1
lird <lird@polyvi.com>
2013-12-18 14:44:22 +0800

    [android]Modify source codes package path

--- src/android/XTelephonyExt.java
+++ src/android/XTelephonyExt.java
@@ -18,7 +18,7 @@
  along with xFace.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.polyvi.xface.extension;
+package com.polyvi.xface.extension.telephony;
 
 import java.util.ArrayList;
 
